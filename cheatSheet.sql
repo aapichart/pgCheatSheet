12da024e04f24202a43320778e3eea5b16dfa9a0
aapichart <chart.asa@gmail.com>
2020-02-09 17:34:37 +0000

    add function skeleton

--- cheatSheet.sql
+++ cheatSheet.sql
@@ -2,8 +2,32 @@
 
 -- list database 
 \l  
+
 -- display tables 
 \d  
--- select tables 
 
+-- select tables 
+select id from receiptno;
+
+-- create or replace function 
+CREATE OR REPLACE FUNCTION firstTest(inValue VARCHAR)
+   RETURNS table(
+    id int,
+    datelog timestamp,
+    stockcode varchar
+    ) AS 
+$$
+DECLARE
+    p record;
+BEGIN
+    FOR p IN 
+        SELECT  from 
+        WHERE () 
+    LOOP
+
+
+        RETURN NEXT;
+    END LOOP;
+END;
+$$ LANGUAGE 'plpgsql';
 
